2c4ff81a851098800728409c0e280ba59ded893a
Jimmi Dyson <jimmidyson@gmail.com>
2016-02-09 14:10:35 +0000

    Fixes #2

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [[ -n "${ELASTICSEARCH_URL}" ]]; then
 elif [[ -n "${ELASTICSEARCH_SERVICE_NAME}" ]]; then
     SVC_HOST=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_HOST
     SVC_PORT=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_PORT
-    sed -i 's|^elasticsearch_url:.*$|elasticsearch_url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana4/config/kibana.yml
+    sed -i 's|^elasticsearch\.url:.*$|elasticsearch.url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana4/config/kibana.yml
 fi
 
 exec /opt/kibana4/bin/kibana
