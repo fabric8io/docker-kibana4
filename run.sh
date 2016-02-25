4a18aeb730b1295028b57772d4b2d3c9ecb9364f
Jimmi Dyson <jimmidyson@gmail.com>
2016-02-25 13:51:15 +0000

    Dodgy sed replacement
    Really fixes #2 this time...

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ -n "${ELASTICSEARCH_URL}" ]]; then
-    sed -i 's|^# elasticsearch\.url:.*$|elasticsearch.url: '"\"${ELASTICSEARCH_URL}\""'|' /opt/kibana4/config/kibana.yml
+    sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"${ELASTICSEARCH_URL}\""'|' /opt/kibana4/config/kibana.yml
 elif [[ -n "${ELASTICSEARCH_SERVICE_NAME}" ]]; then
     SVC_HOST=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_HOST
     SVC_PORT=${ELASTICSEARCH_SERVICE_NAME}_SERVICE_PORT
-    sed -i 's|^elasticsearch\.url:.*$|elasticsearch.url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana4/config/kibana.yml
+    sed -i 's|^\(# \+\)\?elasticsearch\.url:.*$|elasticsearch.url: '"\"http://${!SVC_HOST}:${!SVC_PORT}\""'|' /opt/kibana4/config/kibana.yml
 fi
 
 exec /opt/kibana4/bin/kibana
